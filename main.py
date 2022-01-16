bbcbccbe4011a97db8a995539742653ddbdbc870
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-16 14:51:55 -0800

    created the comments for what to do

--- main.py
+++ main.py
@@ -14,11 +14,20 @@ chosen_word = random.choice(word_list)
 # check if the random word was chosen
 # print(chosen_word)
 
+# 1: - Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+
 # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
 # check if the letter is lower case
 # print(guess)
+
+# 2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in chosen_word:
@@ -26,3 +35,7 @@ for letter in chosen_word:
         print("Right!")
     else:
         print("Wrong!")
+
+
+# 3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
